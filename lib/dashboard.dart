382867dd3078daabdcafbce27a917f8407bf9baf
darmayo <darmayoga1702@gmail.com>
2024-10-26 17:02:11 +0800

    tes

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -5,7 +5,7 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard darma'),
+        title: Text('Dashboard darma ganteng'),
         backgroundColor: Colors.purple,
         actions: [
           IconButton(
