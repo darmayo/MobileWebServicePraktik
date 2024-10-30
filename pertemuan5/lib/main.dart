857fcfff222ea9406d7ed49204b4ad6f86c1c0c0
darmayo <darmayoga1702@gmail.com>
2024-10-30 08:40:26 +0800

    API CRUD

--- pertemuan5/lib/main.dart
+++ pertemuan5/lib/main.dart
@@ -6,7 +6,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://owlegltfdnlmphpkgrxj.supabase.co', // Ganti dengan URL Supabase Anda
+    url: 'https://owlegltfdnlmphpkgrxj.supabase.co', // URL Supabase
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93bGVnbHRmZG5sbXBocGtncnhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4NjY0NTUsImV4cCI6MjA0NTQ0MjQ1NX0.pv9z34zDYse02LdNCcDgYfJoAAPoM8BiS6eF6yyVM04', // Ganti dengan Anon Key Supabase Anda
   );
 
